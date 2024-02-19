6c6bb4bff7311d38ceffe23ee9de335d22d003b8
Emma Holden <emmaholden91@yahoo.com>
2024-02-19 20:01:00 +0000

    console log build num

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css"
 import Home from "./pages/home";
 import ChooseStarter from "./pages/choose-starter";
-import { useContext, useState } from "react";
+import { useState } from "react";
+import packageJson from "../package.json";
 
 function App() {
+    console.log(process.env.REACT_APP_BUILD_NUM)
+    console.log(packageJson.version)
+   
+    console.log(`VERSION: ${packageJson.version}.${process.env.REACT_APP_BUILD_NUM}`);
 
     const [starterChosen, setStarterChosen] = useState(false)
 
