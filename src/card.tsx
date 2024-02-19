d4224f08962bafd361fdde99623cabdd116052b8
Emma Holden <emmaholden91@yahoo.com>
2024-02-19 00:17:20 +0000

    pokemon card styling

--- src/card.tsx
+++ src/card.tsx
@@ -4,33 +4,39 @@ import { Pokemon } from "./pokemonList";
 interface CardProps {
     pokemon: Pokemon;  
     isDisabled?: boolean;
+    starterChosen?: boolean;
+    setStarterChosen?: any;
 }
-const Card = ({pokemon, isDisabled}: CardProps) => {
+const Card = ({pokemon, isDisabled, starterChosen, setStarterChosen}: CardProps) => {
 
     type Stat = 'hp' | 'attack' | 'defense' | 'specialAttack' | 'specialDefense' | 'speed'
 
     const [clicked, setClicked] = useState<Stat[]>([])
+
+    const handleSubmit = () => {
+        setStarterChosen(true)
+    }
 
     const handleClick = (e: any) => {
         let stat: Stat = e.currentTarget.id;
         let newClicked = [...clicked]
         newClicked.push(stat)
         setClicked(newClicked)
-        console.log(pokemon[stat])
     }
 
     return ( 
         <div className = "card">
-            <p>{pokemon.name}</p>
-            <img style = {{width: 150}} src={`../images/${pokemon.id}.png`}></img>
+            <h2>{pokemon.name}</h2>
+            <img className="sprite" src={`../images/${pokemon.id}.png`}></img>
             <div className = "cardButtonGroup">
-                <button id = 'hp' disabled={isDisabled ? true : clicked.includes('hp')} className = 'button-stat' onClick={(e) => handleClick(e)}>HP: {pokemon.hp}</button>
-                <button id = 'attack' disabled={isDisabled ? true : clicked.includes('attack')} className = 'button-stat' onClick={(e) => handleClick(e)}>Attack: {pokemon.attack}</button>
-                <button id = 'defense' disabled={isDisabled ? true : clicked.includes('defense')} className = 'button-stat' onClick={(e) => handleClick(e)}>Defense: {pokemon.defense}</button>
-                <button id = 'specialAttack' disabled={isDisabled ? true : clicked.includes('specialAttack')} className = 'button-stat' onClick={(e) => handleClick(e)}>Special Attack: {pokemon.specialAttack}</button>
-                <button id = 'specialDefense' disabled={isDisabled ? true : clicked.includes('specialDefense')} className = 'button-stat' onClick={(e) => handleClick(e)}>Special Defense: {pokemon.specialDefense}</button>
-                <button id = 'speed' disabled={isDisabled ? true : clicked.includes('speed')} className = 'button-stat' onClick={(e) => handleClick(e)}>Speed: {pokemon.speed}</button>
+                <button id = 'hp' disabled={isDisabled ? true : clicked.includes('hp')} className = {clicked.includes('hp') ? "clicked" : "button-stat"} onClick={(e) => handleClick(e)}>HP: {pokemon.hp}</button>
+                <button id = 'attack' disabled={isDisabled ? true : clicked.includes('attack')} className = {clicked.includes('attack') ? "clicked" : "button-stat"} onClick={(e) => handleClick(e)}>Attack: {pokemon.attack}</button>
+                <button id = 'defense' disabled={isDisabled ? true : clicked.includes('defense')} className = {clicked.includes('defense') ? "clicked" : "button-stat"} onClick={(e) => handleClick(e)}>Defense: {pokemon.defense}</button>
+                <button id = 'specialAttack' disabled={isDisabled ? true : clicked.includes('specialAttack')} className = {clicked.includes('specialAttack') ? "clicked" : "button-stat"} onClick={(e) => handleClick(e)}>Special Attack: {pokemon.specialAttack}</button>
+                <button id = 'specialDefense' disabled={isDisabled ? true : clicked.includes('specialDefense')} className = {clicked.includes('specialDefense') ? "clicked" : "button-stat"} onClick={(e) => handleClick(e)}>Special Defense: {pokemon.specialDefense}</button>
+                <button id = 'speed' disabled={isDisabled ? true : clicked.includes('speed')} className = {clicked.includes('speed') ? "clicked" : "button-stat"} onClick={(e) => handleClick(e)}>Speed: {pokemon.speed}</button>
             </div>
+            {isDisabled && <button onClick = {handleSubmit}>Choose me</button>}
         </div>
      );
 }
--- src/pages/choose-starter.tsx
+++ src/pages/choose-starter.tsx
@@ -2,20 +2,19 @@ import { useState } from "react";
 import Card from "../card";
 import { starterPokemon } from "../pokemonList";
 
+interface ChooseStarterProps {
+    starterChosen: boolean;
+    setStarterChosen: any;
+}
 
-const ChooseStarter = ({starterChosen, setStarterChosen}: {starterChosen: boolean, setStarterChosen: any}) => {
-
-    const handleClick = () => {
-        setStarterChosen(true)
-    }
+const ChooseStarter = ({starterChosen, setStarterChosen}: ChooseStarterProps) => {
     
     return ( 
         <div className = "cardsContainer">
             {starterPokemon.map((pokemon) => {
                 return (
-                    <div className = "starterCardContainer">
-                        <Card pokemon = {pokemon} isDisabled/>
-                        <button onClick = {handleClick}>Choose me</button>
+                    <div>
+                        <Card starterChosen = {starterChosen} setStarterChosen = {setStarterChosen} pokemon = {pokemon} isDisabled/>
                     </div>
                 )
             })
