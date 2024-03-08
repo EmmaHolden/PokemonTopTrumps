b501f5b482c697f47fd86d1081f2ec4469eac01b
Emma Holden <emmaholden91@yahoo.com>
2024-03-08 20:42:59 +0000

    Refactored battle page created new component

--- src/components/battle.tsx
+++ src/components/battle.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pokemon } from "../pokemonList";
 import Button from "./button";
 import CardContainer from "./cardContainer";
 import Name from "./cardName";
 import Sprite from "./cardSprite";
+import InteractiveStats from "./cardInteractiveStats";
 
-type statObj = {
-    "hp": "unselected" | "player" | "computer" | "draw",
-    "attack": "unselected" | "player" | "computer" | "draw",
-    "defense": "unselected" | "player" | "computer" | "draw",
-    "specialAttack": "unselected" | "player" | "computer" | "draw",
-    "specialDefense": "unselected" | "player" | "computer" | "draw",
-    "speed": "unselected" | "player" | "computer" | "draw",
+export type statObj = {
+    "hp": string,
+    "attack": string,
+    "defense": string,
+    "specialAttack": string,
+    "specialDefense": string,
+    "speed": string,
 }
 
 interface BattleProps {
@@ -22,6 +23,9 @@ interface BattleProps {
 const Battle = ({enemyPokemon, playerPokemon}: BattleProps) => {
     const [arrayStats, setArrayStats] = useState({"hp": "unselected", "attack": "unselected", "defense": "unselected", "specialAttack": "unselected", "specialDefense": "unselected", "speed": "unselected"})
     const [playerTurn, setPlayerTurn] = useState(true)
+    const [playerScore, setPlayerScore] = useState(0)
+    const [computerScore, setComputerScore] = useState(0)
+    const [gameOver, setGameOver] = useState(false)
 
     const getRanNum = (min: number, max: number) => {
         return Math.floor(Math.random() * (max - min + 1) + min)
@@ -34,16 +38,19 @@ const Battle = ({enemyPokemon, playerPokemon}: BattleProps) => {
 
     const checkStats = (stat: string) => {
         let newRemainingStats = arrayStats
-        console.log(newRemainingStats)
-        let statCondition = newRemainingStats[stat as keyof statObj]
-        console.log(statCondition)
-        let playerScore = playerPokemon[stat as keyof Pokemon]
-        let computerScore = enemyPokemon[stat as keyof Pokemon]
-        if (playerScore > computerScore){
+        let playerStat = playerPokemon[stat as keyof Pokemon]
+        let computerStat = enemyPokemon[stat as keyof Pokemon]
+        if (playerStat > computerStat){
             newRemainingStats[stat as keyof statObj] = "player"
-        } else if (computerScore > playerScore){
+            let newPlayerScore = playerScore;
+            newPlayerScore += 1;
+            setPlayerScore(newPlayerScore)
+        } else if (computerStat > playerStat){
             newRemainingStats[stat as keyof statObj] = "computer"
-        } else if (computerScore === playerScore){
+            let newComputerScore = computerScore;
+            newComputerScore += 1;
+            setComputerScore(newComputerScore);
+        } else if (computerStat === playerStat){
             newRemainingStats[stat as keyof statObj] = "draw"
         }
         setArrayStats(newRemainingStats) 
@@ -51,146 +58,56 @@ const Battle = ({enemyPokemon, playerPokemon}: BattleProps) => {
     }
 
     const computerTurn = () => {
-        const entries = Object.entries(arrayStats);
-        let availableStats = entries.filter(([key, value]) => value === "unselected");
-        console.log(availableStats.length)
-        let index = getRanNum(0, availableStats.length - 1)
-        console.log("THE ITEM IS:")
-        console.log(availableStats[index][0])
-        checkStats(availableStats[index][0])
-    }
-
-
-    if (playerTurn === false){
         setTimeout(() => {
-            computerTurn()
+            const entries = Object.entries(arrayStats);
+            let availableStats = entries.filter(([key, value]) => value === "unselected");
+            console.log(availableStats.length)
+            let index = getRanNum(0, availableStats.length - 1)
+            checkStats(availableStats[index][0])
         }, 3000);
     }
 
-    const checkImage = (role: string, stat: string) => {
-        if (role === "player"){
-            if (arrayStats[stat as keyof statObj] === "player"){
-                return "../images/check.png"
-            } else if (arrayStats[stat as keyof statObj] === "computer"){
-                return "../images/cross.png"
-            } else if (arrayStats[stat as keyof statObj] === "draw"){
-                return "../images/equal.png"
-            }
-        } else {
-            if (arrayStats[stat as keyof statObj] === "computer"){
-                return "../images/check.png"
-            } else if (arrayStats[stat as keyof statObj] === "player"){
-                return "../images/cross.png"
-            } else if (arrayStats[stat as keyof statObj] === "draw"){
-                return "../images/equal.png"
-            }
 
+    useEffect(() => {
+        const entries = Object.entries(arrayStats);
+        let availableStats = entries.filter(([key, value]) => value === "unselected");
+        if (playerScore > 2 || computerScore > 2 || availableStats.length < 1){
+            setGameOver(true)
+        } else if (playerTurn === false){
+            computerTurn()
         }
+    }, [playerTurn])
 
-    }
 
     return (
         <div>
             <div className = "horizontal-container">
+
                 <Button variant = 'card' className = 'move'>
                     <CardContainer pokemon = {playerPokemon} size = "medium">
                         <Name pokemon = {playerPokemon}/>
                         <Sprite pokemon = {playerPokemon}/>
-                        <div className = "player-stat-button-group">
-                        {arrayStats.hp === "unselected" ?
-                        <Button disabled = {!playerTurn} onClick = {() => checkStats("hp")}variant = "stat" id = "hp"><div className = "separate"><img style = {{width: 30}} src = "../images/pokeballball.png"></img> HP: {playerPokemon.hp}</div></Button> 
-                        :
-                        <Button disabled variant = "stat" id = "stat-revealed"><div className = "separate"><img style = {{width: 30}} src = {checkImage("player", "hp")}></img> HP: {playerPokemon.hp}</div></Button>
-                        }
-
-                        {arrayStats.attack === "unselected" ?
-                        <Button disabled = {!playerTurn} onClick = {() => checkStats("attack")}variant = "stat" id = "attack"><div className = "separate"><img style = {{width: 30}} src = "../images/pokeballball.png"></img> Attack: {playerPokemon.attack}</div></Button> 
-                        :
-                        <Button disabled variant = "stat" id = "stat-revealed"><div className = "separate"><img style = {{width: 30}} src = {checkImage("player", "attack")}></img> Attack: {playerPokemon.attack}</div></Button>
-                        }
-
-                        {arrayStats.defense === "unselected" ?
-                        <Button disabled = {!playerTurn} onClick = {() => checkStats("defense")}variant = "stat" id = "defense"><div className = "separate"><img style = {{width: 30}} src = "../images/pokeballball.png"></img> Defense: {playerPokemon.defense}</div></Button> 
-                        :
-                        <Button disabled variant = "stat" id = "stat-revealed"><div className = "separate"><img style = {{width: 30}} src = {checkImage("player", "defense")}></img> Defense: {playerPokemon.defense}</div></Button>
-                        }
-
-                        {arrayStats.specialAttack === "unselected" ?
-                        <Button disabled = {!playerTurn} onClick = {() => checkStats("specialAttack")}variant = "stat" id = "special-attack"><div className = "separate"><img style = {{width: 30}} src = "../images/pokeballball.png"></img> Sp. Attack: {playerPokemon.specialAttack}</div></Button> 
-                        :
-                        <Button disabled variant = "stat" id = "stat-revealed"><div className = "separate"><img style = {{width: 30}} src = {checkImage("player", "specialAttack")}></img> Sp. Attack: {playerPokemon.specialAttack}</div></Button>
-                        }
-
-                        {arrayStats.specialDefense === "unselected" ?
-                        <Button disabled = {!playerTurn} onClick = {() => checkStats("specialDefense")}variant = "stat" id = "special-defense"><div className = "separate"><img style = {{width: 30}} src = "../images/pokeballball.png"></img>Sp. Defense: {playerPokemon.specialDefense}</div></Button> 
-                        :
-                        <Button disabled variant = "stat" id = "stat-revealed"><div className = "separate"><img style = {{width: 30}} src = {checkImage("player", "specialDefense")}></img>Sp. Defense: {playerPokemon.specialDefense}</div></Button>
-                        }
-
-                        {arrayStats.speed === "unselected" ?
-                        <Button disabled = {!playerTurn} onClick = {() => checkStats("speed")}variant = "stat" id = "speed"><div className = "separate"><img style = {{width: 30}} src = "../images/pokeballball.png"></img>Speed: {playerPokemon.speed}</div></Button> 
-                        :
-                        <Button disabled variant = "stat" id = "stat-revealed"><div className = "separate"><img style = {{width: 30}} src = {checkImage("player", "speed")}></img>Speed: {playerPokemon.speed}</div></Button>
-                        }
-                        </div>
+                        <InteractiveStats gameOver = {gameOver} pokemon = {playerPokemon} handleClick = {checkStats} arrayStats = {arrayStats} playerTurn = {playerTurn} variant = "player"/>
                     </CardContainer>
                 </Button>
+
                         <div className = "vertical-container">
                             <img style = {{width: 300}} src = "../images/vs.png"></img>
+                            {gameOver && <p className = "turn-visible">{playerScore > computerScore ? `${playerPokemon.name} wins!` : `${enemyPokemon.name} wins!`}</p>}
                         </div>
+                        
                 <Button variant = 'card'>
                     <CardContainer pokemon = {enemyPokemon} size = "medium">
                         <Name pokemon = {enemyPokemon}/>
                         <Sprite pokemon = {enemyPokemon}/>
-                        <div>
-                            {arrayStats.hp === "unselected" ?
-                            <Button disabled={true} className = "enemy-stat" variant = "stat"><div className = "separate">HP: ? <img style = {{width: 30}} src = "../images/pokeballball.png"></img> </div></Button>      
-                            :
-                            <Button disabled={true} className = "enemy-stat" variant = "stat" id = 'stat-revealed'><div className = "separate">HP: {enemyPokemon.hp}<img style = {{width: 30}} src = {checkImage("enemy", "hp")}></img> </div></Button>
-
-                            }
-
-                            {arrayStats.attack === "unselected" ?
-                            <Button disabled={true} className = "enemy-stat" variant = "stat"><div className = "separate">Attack: ?<img style = {{width: 30}} src = "../images/pokeballball.png"></img></div></Button>
-                            :
-                            <Button disabled={true} className = "enemy-stat" variant = "stat" id = 'stat-revealed'><div className = "separate">Attack: {enemyPokemon.attack}<img style = {{width: 30}} src = {checkImage("enemy", "attack")}></img> </div></Button>
-                            }
-
-                            {arrayStats.defense === "unselected" ?
-                            <Button disabled={true} className = "enemy-stat" variant = "stat"><div className = "separate">Defense: ?<img style = {{width: 30}} src = "../images/pokeballball.png"></img></div></Button>
-                            :
-                            <Button disabled={true} className = "enemy-stat" variant = "stat" id = 'stat-revealed'><div className = "separate">Defense: {enemyPokemon.defense}<img style = {{width: 30}} src = {checkImage("enemy", "defense")}></img> </div></Button>
-                            }
-
-                            {arrayStats.specialAttack === "unselected" ?
-                            <Button disabled={true} className = "enemy-stat" variant = "stat"><div className = "separate">Sp. Attack: ?<img style = {{width: 30}} src = "../images/pokeballball.png"></img></div></Button>
-                            :
-                            <Button disabled={true} className = "enemy-stat" variant = "stat" id = 'stat-revealed'><div className = "separate">Sp. Attack: {enemyPokemon.specialAttack}<img style = {{width: 30}} src = {checkImage("enemy", "specialAttack")}></img> </div></Button>
-                            }
-
-                            {arrayStats.specialDefense === "unselected" ?
-                            <Button disabled={true} className = "enemy-stat" variant = "stat"><div className = "separate">Sp. Defense: ?<img style = {{width: 30}} src = "../images/pokeballball.png"></img></div></Button>
-                            :
-                            <Button disabled={true} className = "enemy-stat" variant = "stat" id = 'stat-revealed'><div className = "separate">Sp. Defense: {enemyPokemon.specialDefense}<img style = {{width: 30}} src = {checkImage("enemy", "specialDefense")}></img> </div></Button>
-                            }
-
-                            
-                            {arrayStats.speed === "unselected" ?
-                            <Button disabled={true} className = "enemy-stat" variant = "stat"><div className = "separate">Speed: ?<img style = {{width: 30}} src = "../images/pokeballball.png"></img></div></Button>
-                            :
-                            <Button disabled={true} className = "enemy-stat" variant = "stat" id = 'stat-revealed'><div className = "separate">Speed: {enemyPokemon.speed}<img style = {{width: 30}} src = {checkImage("enemy", "speed")}></img> </div></Button>
-                            }
-
-                            
-                            
-                        </div>
+                        <InteractiveStats gameOver = {gameOver} pokemon = {enemyPokemon} handleClick = {checkStats} arrayStats = {arrayStats} playerTurn = {playerTurn} variant = "enemy"/>
                     </CardContainer>
                 </Button>
             </div>
             <div className = "horizontal-container margin-top-ten">
-                <div className = "third-horizontal-container"><img className = {!playerTurn ? "rotating-pokeball paused" : "rotating-pokeball"} style = {{width: 100}} src = "../images/pokeballball.png"></img><p className = {!playerTurn ? "turn-invisible" : "turn-visible"}>{`${playerPokemon.name}'s turn`}</p></div>
-                <div className = "second-horizontal-container"><img style = {{height: 100}} src = {`../images/number${1}.png`}></img><img style = {{height: 100}} src = "../images/dash.png"></img><img style = {{height: 100}} src = {`../images/number${2}.png`}></img></div>
-                <div className = "third-horizontal-container"><p className = {playerTurn ? "turn-invisible" : "turn-visible"}>{`${enemyPokemon.name}'s turn`}</p><img className = {playerTurn ? "rotating-pokeball paused" : "rotating-pokeball"} style = {{width: 100}} src = "../images/pokeballball.png"></img></div>
+                {!gameOver && <div className = "third-horizontal-container"><img className = {!playerTurn ? "rotating-pokeball paused" : "rotating-pokeball"} style = {{width: 100}} src = "../images/pokeballball.png"></img><p className = {!playerTurn ? "turn-invisible" : "turn-visible"}>{`${playerPokemon.name}'s turn`}</p></div>}
+                <div className = "second-horizontal-container"><img style = {{height: 100}} src = {`../images/number${playerScore}.png`}></img><img style = {{height: 100}} src = "../images/dash.png"></img><img style = {{height: 100}} src = {`../images/number${computerScore}.png`}></img></div>
+                {!gameOver && <div className = "third-horizontal-container"><p className = {playerTurn ? "turn-invisible" : "turn-visible"}>{`${enemyPokemon.name}'s turn`}</p><img className = {playerTurn ? "rotating-pokeball paused" : "rotating-pokeball"} style = {{width: 100}} src = "../images/pokeballball.png"></img></div>}
             </div>
         </div>
     )
